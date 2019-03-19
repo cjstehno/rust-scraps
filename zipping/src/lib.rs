c2a1c37d616ecee26350fc5078f336e839a787ee
Chris Stehno <chris.stehno@netscout.com>
2019-03-19 09:54:25 -0500

    first pass at working zip create for file or dir

--- zipping/src/lib.rs
+++ zipping/src/lib.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 
-use zip::result::ZipResult;
+use zip::result::{ZipError, ZipResult};
 use zip::write::FileOptions;
 use zip::ZipWriter;
 
@@ -32,10 +32,33 @@ fn create_zip_file(path: &Path, zip_file: &File) -> ZipResult<()> {
         file.read_to_end(&mut buffer)?;
         writer.write_all(&*buffer)?;
         buffer.clear();
-    } else {
+    } else if path.is_dir() {
         // zip a directory of files
-        unimplemented!("Directory zip is not yet implemented.");
-    };
+        let mut directories = vec![path.to_path_buf()];
+
+        while !directories.is_empty() {
+            let directory = directories.pop().unwrap();
+            writer.add_directory(directory.to_str().unwrap(), options)?;
+
+            for dir_entry in directory.read_dir().unwrap() {
+                let entry = dir_entry.unwrap();
+                let file_type = entry.file_type().unwrap();
+
+                if file_type.is_dir() {
+                    directories.push(entry.path());
+                } else if file_type.is_file() {
+                    writer.start_file(directory.join(entry.file_name().to_str().unwrap()).to_str().unwrap(), options)?;
+
+                    let mut file = File::open(entry.path()).unwrap();
+                    file.read_to_end(&mut buffer)?;
+                    writer.write_all(&*buffer)?;
+                    buffer.clear();
+                }
+            }
+        }
+    } else {
+        return Err(ZipError::FileNotFound);
+    }
 
     writer.finish()?;
 
@@ -58,6 +81,23 @@ mod tests {
         let zip_file = File::create(&zip_path).unwrap();
 
         let path = Path::new("rc/file-a.txt");
+
+        create_zip_file(path, &zip_file).expect("Failed to create zip file!");
+
+        assert_eq!(zip_path.is_file(), true);
+        assert_eq!(zip_path.exists(), true);
+        assert_eq!(zip_file.metadata().unwrap().len() > 0, true);
+
+        // TODO: assert that it has the specified contents
+    }
+
+    #[test]
+    fn zip_directory_of_files() {
+        let temp_dir = TempDir::new().unwrap();
+        let zip_path = temp_dir.path().join("multi-file.zip");
+        let zip_file = File::create(&zip_path).unwrap();
+
+        let path = Path::new("rc/");
 
         create_zip_file(path, &zip_file).expect("Failed to create zip file!");
 
